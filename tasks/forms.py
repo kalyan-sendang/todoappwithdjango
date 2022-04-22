1f1235fc5f8321177f5d4e3744af30fab3cfe5b7
kalyan-sendang <kalyansendang10@gmail.com>
2022-04-22 13:26:51 +0545

    first commit

---
+++ tasks/forms.py
@@ -0,0 +1,16 @@
+from asyncio import tasks
+from dataclasses import fields
+import imp
+from importlib.metadata import metadata
+from django import forms
+from django.forms import ModelForm
+
+from .models import *
+
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new tasks....'}))
+    class Meta:
+        model= Task
+        fields = '__all__'
+
+ 
